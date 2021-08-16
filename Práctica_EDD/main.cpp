836d3cc3e9a915d55144e9af231e5e611350201a
Magdiel Asicona <3275257541405@ingenieria.usac.edu.gt>
2021-08-16 01:14:09 -0600

    Menu principal

--- Práctica_EDD/main.cpp
+++ Práctica_EDD/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+#include "Menu.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    menuPrincipal();
     return 0;
 }
---
+++ Práctica_EDD/src/Menu.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Menu.h"
+#include <string>
+using namespace std;
+void menuPrincipal(){
+    string opcionPrincipal;
+printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c \n",201,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,187);
+printf("%c        MENU PRINCIPAL       %c\n",186,186);
+printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",200,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,188);
+printf("\n");
+printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c \n",201,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,187);
+printf("%c  [1] Carga de Estudiantes   %c\n",186,186);
+printf("%c  [2] Carga de Tareas        %c\n",186,186);
+printf("%c  [3] Ingreso Manual         %c\n",186,186);
+printf("%c  [4] Reportes               %c\n",186,186);
+printf("%c  [5] Salir                  %c\n",186,186);
+printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",200,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,205,188);
+
+cout << "Ingrese una opcion >> ";
+getline(cin,opcionPrincipal);
+
+//opciones del menu
+if(opcionPrincipal == "1"){
+cout << "Carga de estudiantes" << endl;
+}else if(opcionPrincipal == "2"){
+ cout << "Carga de tareas" << endl;
+}else if(opcionPrincipal == "3"){
+ cout << "Ingreso manual" << endl;
+}else if(opcionPrincipal == "4"){
+ cout << "Reportes" << endl;
+}else if(opcionPrincipal == "5"){
+ exit(0);
+}else{
+
+    cout << "ERROR: Al menos seleccione una opcion" << endl;
+    cout << "\n" << endl;
+menuPrincipal();
+}
+}
+
+
