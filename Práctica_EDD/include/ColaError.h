1950c16ebae8463cade6919147adfc11a3b1df4d
Magdiel Asicona <3275257541405@ingenieria.usac.edu.gt>
2021-08-19 23:42:11 -0600

    Creacion cola

---
+++ Práctica_EDD/include/ColaError.h
@@ -0,0 +1,14 @@
+#ifndef COLAERROR_H
+#define COLAERROR_H
+
+
+class ColaError
+{
+    public:
+        ColaError();
+        virtual ~ColaError();
+
+
+};
+
+#endif // COLAERROR_H
---
+++ Práctica_EDD/src/ColaError.cpp
@@ -0,0 +1,11 @@
+#include "ColaError.h"
+
+ColaError::ColaError()
+{
+    //ctor
+}
+
+ColaError::~ColaError()
+{
+    //dtor
+}
