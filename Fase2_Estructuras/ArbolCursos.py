ffb79b0a761d8b282429a1b61c4aa48764942ba5
Magdiel Asicona <3275257541405@ingenieria.usac.edu.gt>
2021-09-22 01:02:44 -0600

    Grafica arbol b

--- Fase2_Estructuras/ArbolCursos.py
+++ Fase2_Estructuras/ArbolCursos.py
@@ -1,5 +1,6 @@
-from abc import ABC
 
+import os
+import sys
 
 class Nodo:
     def __init__(self,_codigo,_nombre,_noCreditos,_codPrerrequisito,_obligatorio):
@@ -30,8 +31,8 @@ class Rama:
                 if(nodo.codigo <= actual.codigo):
                     self.contador +=1
                     if(actual==self.raiz):
+                        nodo.siguiente = self.raiz
                         self.raiz.anterior = nodo
-                        self.raiz.izquierdo = nodo.derecho
                         self.raiz = nodo
                         break
                     else:
@@ -61,6 +62,7 @@ class Arbolb:
         self.orden = 5
 
     def insertar(self,_codigo,_nombre,_noCreditos,_codPrerrequisito,_obligatorio):
+        _codigo = int(_codigo)
         nodo = Nodo(_codigo,_nombre,_noCreditos,_codPrerrequisito,_obligatorio)
         if(self.raiz == None):
             self.raiz = Rama()
@@ -147,8 +149,16 @@ class Arbolb:
         medio.derecho = derecha
        
         return medio
+        
     def buscarNodo(self,_codigo):
-        self.buscar(_codigo,self.raiz)
+        try:
+            _codigo = int(_codigo)
+            aux = self.buscar(_codigo,self.raiz)
+            if(aux!=None):
+                 print("encontrado")
+    
+        except AttributeError:
+            print("No encontrado")
         
 
     def buscar(self,_codigo,actual):
@@ -158,7 +168,7 @@ class Arbolb:
 
         if(_codigo == actual.codigo):
             print(actual.codigo)
-            return actual.codigo
+            
         elif(_codigo < actual.codigo):
             actual = actual.izquierdo
             self.buscar(_codigo,actual)
@@ -169,32 +179,109 @@ class Arbolb:
             else:
                 actual = actual.siguiente
                 self.buscar(_codigo,actual)
-        
+       
+        return actual
 
+    def graficar(self):
+        name = "arbolb"
+        file = open("Graficas/"+name+".dot","w",encoding="UTF-8")
+        file.write("digraph G{\n")
+        file.write("rankdor = TB;\n")
+        file.write("splines=line;\n")
+        file.write('node[shape = record,style="rounded,filled",fillcolor=lightblue2];\n')
+        #los nodos
+        file.write(self.graficarNodos(self.raiz))
+        file.write(self.graficarEnlaces(self.raiz))
+        file.write("\n")
+        file.write("}\n")
+        file.close()
+        os.system("dot -Tsvg Graficas/"+name+".dot -o Graficas/"+name+".svg")
 
+    def graficarNodos(self,raiz_actual):
+        grafo =""
+        if(raiz_actual.hoja):
+            grafo+='node[label="<C0>'
+            cont = 0
+            pivote = raiz_actual.raiz
+            while(pivote!=None):
+                cont+=1
+                grafo += "|{"+str(pivote.codigo)+"\\n"+str(pivote.nombre)+"}|<C"+str(cont)+">"
+                pivote = pivote.siguiente
+            
+            grafo += '"]'+str(raiz_actual.raiz.codigo)+";\n"
+            return grafo
+        else:
+            grafo+="node[label=\"<C0>"
+            cont = 0
+            pivote = raiz_actual.raiz
 
+            while(pivote!=None):
+                cont+=1
+                grafo+="|{"+str(pivote.codigo)+"\\n"+str(pivote.nombre)+"}|<C"+str(cont)+">"
+                pivote = pivote.siguiente
+             
+            grafo+="\"]"+str(raiz_actual.raiz.codigo)+";\n"
 
-
+            pivote = raiz_actual.raiz
+            cont = 0
+            llave = False
+            while(pivote!=None):
+                if(pivote.siguiente==None):
+                    llave = True
+                aux = ""
+                aux+=self.graficarNodos(pivote.izquierdo)
+                grafo += aux
+                cont+=1
                 
+                if(llave == True):
+                    grafo += self.graficarNodos(pivote.derecho)
+                
+                pivote = pivote.siguiente
+
+            
+            return grafo 
+
+    def graficarEnlaces(self,raiz_actual):
+        grafo = ""
+        if(raiz_actual.hoja):
+            return ""+str(raiz_actual.raiz.codigo)+";"
+        else:
+            grafo += ""+str(raiz_actual.raiz.codigo)+";"
+            pivote = raiz_actual.raiz
+            cont = 0
+            actual = ""+str(raiz_actual.raiz.codigo)
+            llave = False
+            while(pivote!=None):
+                if(pivote.siguiente==None):
+                    llave = True
+                grafo+="\n"+actual+":C"+str(cont)+"->"+self.graficarEnlaces(pivote.izquierdo)
+                cont+=1
+                if(llave == True):
+                    grafo += "\n"+actual+":C"+str(cont)+"->"+self.graficarEnlaces(pivote.derecho)
+                pivote = pivote.siguiente
+
+            #grafo += "\n"+actual+":C"+str(cont)+"->"+self.graficarEnlaces(raiz_actual.raiz.derecho)
+            return grafo 
+ 
 b = Arbolb()
-b.insertar(10,"Juna",15,125,True)
-b.insertar(11,"Pedro",22,126,False)
-b.insertar(12,"Kass",32,127,True)
-b.insertar(13,"Pina",12,128,False)
-b.insertar(14,"Sandia",43,129,True)
-b.insertar(15,"Limon",23,130,False)
-b.insertar(16,"Mesa",231,131,True)
-b.insertar(17,"Mouse",31,132,False)
-b.insertar(18,"Lapiz",61,133,True)
-b.insertar(19,"PC",33,134,False)
-b.insertar(20,"Dell",11,135,True)
-b.insertar(21,"Mac",33,134,False)
-b.insertar(22,"Hp",11,135,True)
-b.insertar(23,"Lis",33,134,False)
-b.insertar(24,"Too",11,135,True)
-b.insertar(25,"Vig",33,134,False)
-b.insertar(26,"Mar",11,135,True)
-b.buscarNodo(26)
+b.insertar(101,"Matematica Basica 1",7,"",True)
+b.insertar(39,"Deportes 1",1,"",False)
+b.insertar(348,"Quimica General 1",3,"",True)
+b.insertar(103,"Matematica Basica 2",7,"101",True)
+b.insertar(5,"Tecnias de Estudio e Investigacion",3,"",True)
+b.insertar(960,"Matematica Computo 1",5,"103",True)
+b.insertar(107,"Metematica Intermedia 1",10,"103",True)
+b.insertar(770,"Introduccion a la Programacion y Computacion 1",4,"103",True)
+b.insertar(796,"Lenguajes Formales y de Programación",3,"770,795,960",True)
+b.insertar(772,"Estructuras de Datos",5,"771,796,962",True)
+b.insertar(777,"Organización de Lenguajes y Compiladores 1",4,"771,796,962",True)
+b.insertar(18,"Filosofia de la Ciencia",3,"019",False)
+b.insertar(774,"Sistemas de Bases de Datos 1",5,"773",True)
+b.insertar(281,"Sistemas Operativos 1",4,"781,778",True)
+b.insertar(2036,"Practica Intermedia",0,"778,777,773,2025",True)
+b.buscarNodo(2033)
+b.graficar()
+#b.buscarNodo(26)
 
 
                 
