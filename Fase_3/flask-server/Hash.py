061daa58e35dea5b010bc1b2bd322d1a87b8549a
Magdiel Asicona <3275257541405@ingenieria.usac.edu.gt>
2021-11-03 22:09:24 -0600

    Tabla hash completado

--- Fase_3/flask-server/Hash.py
+++ Fase_3/flask-server/Hash.py
@@ -1,11 +1,15 @@
+import math
+import os
+import sys
+
 class Nodo:
     def __init__(self, indice):
         self.indice = indice
         self.lista = []
 
 class Llave:
-    def __init__(self,indice, titulo, contenido):
-        self.indice = indice
+    def __init__(self,carnet, titulo, contenido):
+        self.carnet = carnet
         self.titulo = titulo
         self.contenido = contenido
     def print(self):
@@ -24,11 +28,21 @@ class Hash:
     def insertar(self, id, titulo, contenido):
         posicion = self.funcion_hash(id)
         
-        if self.vector[posicion] != None:
-            nuevo_contenido = Llave(id,titulo,contenido)
-            self.vector[posicion].lista.append(nuevo_contenido)
-        else:
-            nuevo = Nodo(posicion)
+        if self.vector[posicion] != None: #cuando ya existe el nodo principal
+            listas = self.vector[posicion].lista
+            _carnet = listas[0].carnet
+            if _carnet == id: #si el carnet es igual al ingresado
+                nuevo_contenido = Llave(id,titulo,contenido)
+                self.vector[posicion].lista.append(nuevo_contenido)
+            else:#si el carnet es nuevo pero se colisiona, pues exploracion
+                nueva_posicion = self.exploracion(posicion)
+                nuevo = Nodo(nueva_posicion)
+                nuevo.lista.append(Llave(id,titulo, contenido))
+                self.vector[nueva_posicion] = nuevo
+                self.elementos+=1
+                self.factorCarga = self.elementos/self.tamano
+        else: #cuando se crea el nodo principal
+            nuevo = Nodo(id)
             nuevo.lista.append(Llave(id,titulo, contenido))
             self.vector[posicion] = nuevo
             self.elementos+=1
@@ -37,12 +51,41 @@ class Hash:
         if self.factorCarga > 0.5:
             self.rehashing()
 
+    def esPrimo(self,num):
+        if num < 1:
+            return False
+        elif num == 2:
+            return True
+        else:
+            for i in range(2, num):
+                if num % i == 0:
+                    return False
+            return True  
+
+    def exploracion(self,id):
+        indice=0
+        i =0
+        disponible = False
+        while(disponible==False):
+            indice = id + int(pow(i,2))
+            if(indice>= self.tamano):
+                indice = indice - self.tamano
+
+            if(self.vector[indice]==None):
+                disponible = True
+
+            i+=1
+
+        return indice
+
     def rehashing(self):
         siguiente = self.tamano
-        factor = 0
-        while(factor < 0.3):
-            factor = self.elementos/siguiente
-            siguiente +=1
+        bandera = False
+
+        while(bandera==False):
+            siguiente+=1
+            bandera = self.esPrimo(siguiente)
+            
 
         temporal = []
         self.elementos = 0
@@ -59,13 +102,11 @@ class Hash:
         for i in aux_vector:
             if i:
                 for j in i.lista:
-                    self.insertar(j.indice, j.titulo, j.contenido)
+                    self.insertar(j.carnet, j.titulo, j.contenido)
 
 
     def funcion_hash(self, id):
-        posicion = id % (self.tamano-1)
-        if posicion > self.tamano:
-            return posicion - self.tamano
+        posicion = id % self.tamano
         return posicion
 
 
@@ -80,18 +121,56 @@ class Hash:
 
             contador+=1
 
-def toASCII(cadena):
-    result = 0
-    for char in cadena:
-        result += ord(char)
-    return result
+    def graficar(self):
+        file = open("C:\\Users\\Magdiel\\Desktop\\EDD_SmartClass_201801449\\Fase_3\\Reportes\\hash.dot","w",encoding="UTF-8")
+        file.write("digraph G{\n")
+        file.write("rankdir=LR;\n")
+        file.write("splines=line;\n")
+        file.write('node[shape = record,style="filled",fillcolor=lightblue2,height=0];\n')
 
+        #recorro primero el vector, enlisto los carnet
+        contador=0
+        claves = ""
+        for i in self.vector:
+            contador+=1
+            if contador != len(self.vector):
+                if i !=None:
+                    aux = i
+                    claves+= '<c'+str(aux.indice)+'> '+str(aux.indice)+"|"
+                else:
+                    claves+= "|"
+                
+        
+        file.write('"hash"[label="'+claves+'"];\n')
 
-tabla = Hash()
-tabla.insertar(201801449,"primero","ocupado")
-tabla.insertar(202102237,"casa","colision")
-tabla.insertar(201801449,"saca","colision")
-tabla.insertar(201501456,"saca1","colision")
-tabla.insertar(202102234,"saca2","no hay colision")
-tabla.insertar(201801440,"saca3","1")
-tabla.print()
+        #luego recorro la lista, de la lista
+        count = 0
+        bandeNodo = True
+        anterior = ""
+        for m in self.vector:
+            bandeNodo = True
+            if m!=None:
+                for n in m.lista:
+                    if bandeNodo == True:
+                        file.write('"'+str(n.titulo)+'"[label="'+str(n.titulo)+'"];\n')
+                        file.write('"hash":c'+str(m.indice)+"->"+str(n.titulo)+';\n')
+                        anterior = n.titulo
+                        bandeNodo = False
+                    else:
+                        file.write('"'+str(n.titulo)+'"[label="'+str(n.titulo)+'"];\n')
+                        file.write(str(anterior)+"->"+str(n.titulo)+";\n")
+                        anterior = n.titulo
+        file.write("}\n")
+        file.close()
+        os.system("dot -Tpng C:\\Users\\Magdiel\\Desktop\\EDD_SmartClass_201801449\\Fase_3\\Reportes\\hash.dot -o C:\\Users\\Magdiel\\Desktop\\EDD_SmartClass_201801449\\Fase_3\\Reportes\\hash.png")
+
+""" tabla = Hash()
+tabla.insertar(2,"primero","ocupado")
+tabla.insertar(9,"casa","colision")
+tabla.insertar(16,"saca","colision")
+tabla.insertar(23,"saca1","colision")
+tabla.insertar(2,"saca2","no hay colision")
+tabla.insertar(2,"saca3","1")
+tabla.insertar(16,"sacaasd","colision")
+tabla.insertar(16,"sacafdsf","colision")
+tabla.graficar()  """
